ba589d8a3afd5765f1fa3b6c5a758532f846ef3b
dengyunsheng <515857422@qq.com>
2021-01-08 21:42:39 +0800

    增加person

---
+++ src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther: dengyunsheng
+ * @Date: 2021/1/8 - 01 - 08 - 21:41
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
